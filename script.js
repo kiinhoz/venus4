6deaaf9a1e7fa3bc119bfde818ddfc98419fe24c
kiim <kinroshima2@gmail.com>
2023-06-14 13:52:03 -0300

    cesta e home

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ let search = document.querySelector('.search');
 
 document.querySelector('#search-bar').onclick  = () => {
     search.classList.toggle('active');
+    shoppingcart.classList.remove('active');
+
+
+}
+
+let shoppingcart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick  = () => {
+    shoppingcart.classList.toggle('active');
+    search.classList.remove('active');
+
 }
