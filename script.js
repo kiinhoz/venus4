86958e4e0c7c3961c4cd287231adc8aefc878f91
kiim <kinroshima2@gmail.com>
2023-06-15 06:46:06 -0300

    banner

--- script.js
+++ script.js
@@ -12,5 +12,30 @@ let shoppingcart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick  = () => {
     shoppingcart.classList.toggle('active');
     search.classList.remove('active');
-
 }
+
+var swiper = new Swiper('.produto-slider',{
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+        
+    },
+});
